4761cbc1eb3c127a120772480bab7ebe2de817f5
nikita <paramonov20@bk.ru>
2023-04-12 05:10:27 +0300

    dataclass

--- src/main/kotlin/dataclass/homework/MyDataClassAddress.kt
+++ src/main/kotlin/dataclass/homework/MyDataClassAddress.kt
@@ -16,4 +16,28 @@ class MyDataClassAddress(
     ): MyDataClassAddress {
         return MyDataClassAddress(nameCity, streetName, numberOfHouse)
     }
+
+    override fun toString(): String {
+        return "nameCity: $nameCity, streetName: $streetName, numberOfHouse: $numberOfHouse"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MyDataClassAddress
+
+        if (nameCity != other.nameCity) return false
+        if (streetName != other.streetName) return false
+        if (numberOfHouse != other.numberOfHouse) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = nameCity.hashCode()
+        result = 31 * result + streetName.hashCode()
+        result = 31 * result + numberOfHouse.hashCode()
+        return result
+    }
 }
--- src/main/kotlin/method/homework/main.kt
+++ src/main/kotlin/method/homework/main.kt
@@ -3,10 +3,9 @@ package method.homework
 fun main() {
     val employee = Employee(name = "Nikita", jobTitle = "Android Developer", startYear = 2022)
     employee.work()
-    println(employee.experience)
     employee.printInfo()
 }
 
 fun Employee.printInfo() {
-    println("Name: $name, JobTitle: $jobTitle, Year: $startYear")
+    println("Name: $name, JobTitle: $jobTitle, Year: $startYear, Experience: $experience")
 }
