d8db7f03f9b41caff251e4525e84914ce8b511ea
nikita <paramonov20@bk.ru>
2023-04-12 03:52:53 +0300

    equals

---
+++ src/main/kotlin/equals/java/Main.java
@@ -0,0 +1,13 @@
+package equals.java;
+
+public class Main {
+    public static void main(String[] args) {
+        Address address = new Address("Moscow", "Prospect", 78);
+        Address address1 = new Address("Moscow", "Prospect", 78);
+        if (address1.equals(address)) {
+            System.out.println("Равны");
+        } else {
+            System.out.println("Не равны");
+        }
+    }
+}
