edb043c2d82c56748c65444da59347468e2d5d84
nikita <paramonov20@bk.ru>
2023-04-21 04:27:26 +0300

    Generic

---
+++ src/main/kotlin/generic/example/example.kt
@@ -0,0 +1,14 @@
+package generic.example
+
+fun main() {
+    val list: MyList<Int> = MyArrayList()
+    for (i in 0..100) {
+        list.add(i)
+    }
+    for (i in 10..100) {
+        list.removeAt(10)
+    }
+    for (i in 0 until list.size()) {
+        println(list.get(i))
+    }
+}
